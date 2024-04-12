eaa277b36235a8243c0f790b45af7e62c0624f70
Виктор Рябоконь <vr9@Viktors-MacBook-Pro.local>
2024-04-12 02:54:01 -0400

    test with text

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { TextFile } from './Text';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -34,7 +35,8 @@ const router = createBrowserRouter([
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <TextFile />
+    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>
 );
 
